5c12c4f29198aaf1ef218639ff12fee8bd32689b
Derek Hassick <derekhassick@Dereks-MacBook-Air.local>
2018-07-12 01:02:34 -0600

    added deets div and cards are styled

--- src/Components/VehCard.js
+++ src/Components/VehCard.js
@@ -3,11 +3,13 @@ import { Link } from 'react-router-dom';
 
 function VehCard(props) {
   return (
-    < div className="vehicle-card" id={props.veh.id} >
-      <p className="make" >Make : {props.veh.make} </p>
-      <p className="model" >Model: {props.veh.model}</p>
-      <p className="year" >Year: {props.veh.year}</p>
-      <p className="details" >Details: {props.veh.note}</p>
+    <div className="vehicle-card" id={props.veh.id} >
+      <div className="veh-deets">
+        <p className="make" >Make : {props.veh.make} </p>
+        <p className="model" >Model: {props.veh.model}</p>
+        <p className="year" >Year: {props.veh.year}</p>
+        <p className="details" >Details: {props.veh.note}</p>
+      </div>
       <div className="veh-buttons">
         <Link to="/Log" >
           <button className="veh-button" onClick={(event) => props.onClickedCard(props.veh.id)}>
@@ -17,7 +19,7 @@ function VehCard(props) {
         <button className="veh-button" onClick={(event) => props.handleEdit(props.veh.id)}>Edit</button>
         <button className="veh-button" onClick={(event) => props.handleDelete(props.veh.id)}>Delete</button>
       </div>
-    </div >
+    </div>
   )
 }
 
