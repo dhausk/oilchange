f23016d21b75f2a327a6251a4db312fb21ff4db8
Derek Hassick <derekhassick@Dereks-MacBook-Air.local>
2018-07-11 23:54:41 -0600

    splash page is styled

--- src/Components/Splash.js
+++ src/Components/Splash.js
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 
 function Splash() {
   return (
-    < section className="" >
-      <h1 className="">Oil Change</h1>
+    <section className="splash" >
+      <h1>Oil Change</h1>
       <p>If you have ever forgotten when you changed the oil, simply ran out of room or lost your
       maintenance log book. Dont worry you are not alone, and fortunately there is Oil Change, an
       app that will be the maintenance log book in your pocket. All you need to do. Just pick out
@@ -13,10 +13,10 @@ function Splash() {
       data to give a time line of when you last did your maintenance and the frequency. It is simple
       to use, fill out a form and we will do the rest. Do an oil change? Change the air Filter?
       Just fill out another form an we will keep a record for you.</p>
-      <button>
-        <Link to="/Vehicles">Begin</Link>
-      </button>
-    </section >
+      <Link to="/Vehicles">
+        <button>Let's do some Work</button>
+      </Link>
+    </section>
   )
 }
 
