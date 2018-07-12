b251c5f7bcb14999772dbe31c7774b752b0fdf0a
Derek Hassick <derekhassick@Dereks-MacBook-Air.local>
2018-07-12 00:24:15 -0600

    about style fixed added div to about n pic, fixed background color

--- src/Components/About.js
+++ src/Components/About.js
@@ -9,7 +9,7 @@ function About(params) {
         <img alt="Derek Hassick" src={me} />
         <p>Hello, My Name is Derek Hassick and I am a web developer who has a hankering to do
         my own vehicle maintenance. I created this app to help me keep track of all maintenance I do. I hope it
-      you find it helps you as well.</p>
+      you find it just as helpful as I do.</p>
       </div>
       <Link to="/"><button>Happy Tinkering</button></Link>
     </section>
