ad2d9bc539ab73d7d5d1c322840b763930aac394
Derek Hassick <derekhassick@Dereks-MacBook-Air.local>
2018-07-12 23:08:40 -0600

    add is passing completed form

--- src/Components/vehForm/AddVeh.js
+++ src/Components/vehForm/AddVeh.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
 
-class EditVeh extends Component {
+class addVeh extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      "id": 1,
       "make": "",
       "model": "",
-      "year": 1111,
+      "year": "",
       "note": ""
     }
   }
@@ -17,22 +16,21 @@ class EditVeh extends Component {
     this.setState({
       [key]: value
     })
-    console.log(this.state);
-
   }
+
   render() {
     return (
       <div className="update-veh">
         <h4>Add a Vehicle</h4>
-        <form className="form" onSubmit={(event) => this.props.formSubmit(event, this.state)}>
+        <form className="form" onSubmit={(event) => this.props.handleAdd(event, this.state)}>
           <label name="make">Vehicle Make</label>
-          <input type="text" name="make" onChange={this.changeHandler} placeholder="Make of the vehicle" required ></input>
+          <input type="text" name="make" onChange={this.changeHandler} placeholder="Make of the vehicle" value={this.state.make} required ></input>
           <label name="model">Model of Vehicle</label>
-          <input type="text" name="model" onChange={this.changeHandler} placeholder="Model of vehicle" required ></input>
+          <input type="text" name="model" onChange={this.changeHandler} placeholder="Model of vehicle" value={this.state.model} required ></input>
           <label name="year">Year of Manufacture</label>
-          <input type="text" name="year" onChange={this.changeHandler} placeholder="YYYY" minLength="4" maxLength="4" ></input>
+          <input type="text" name="year" onChange={this.changeHandler} placeholder="YYYY" minLength="4" value={this.state.year} maxLength="4" ></input>
           <label name="note">Any additional comments or notes</label>
-          <textarea name="note" onChange={this.changeHandler} placeholder="Add any additional info like transmission type, engine size, sedan or coupe, color." ></textarea>
+          <textarea name="note" onChange={this.changeHandler} placeholder="Add any additional info like transmission type, engine size, sedan or coupe, color." value={this.state.note}></textarea>
           <button className="form-submit"> Submit</button>
         </form>
       </div>
@@ -40,4 +38,4 @@ class EditVeh extends Component {
   }
 }
 
-export default EditVeh;
+export default addVeh;
--- src/Components/vehForm/updateVeh.js
+++ src/Components/vehForm/updateVeh.js
@@ -7,13 +7,13 @@ class updateVeh extends Component {
         <h4>Update this Vehicle</h4>
         <form className="form" onSubmit={(event) => this.props.handleEdit(event, this.props.selectedCard.id)}>
           <label name="make">Vehicle Make</label>
-          <input type="text" name="make" onChange={this.changeHandler} defaultValue={this.props.selectedCard.make} required ></input>
+          <input type="text" name="make" defaultValue={this.props.selectedCard.make} required ></input>
           <label name="model">Model of Vehicle</label>
-          <input type="text" name="model" onChange={this.changeHandler} defaultValue={this.props.selectedCard.model} required ></input>
+          <input type="text" name="model" defaultValue={this.props.selectedCard.model} required ></input>
           <label name="year">Year of Manufacture</label>
-          <input type="text" name="year" onChange={this.changeHandler} defaultValue={this.props.selectedCard.year} minLength="4" maxLength="4" ></input>
+          <input type="text" name="year" defaultValue={this.props.selectedCard.year} minLength="4" maxLength="4" ></input>
           <label name="note">Any additional comments or notes</label>
-          <textarea name="note" onChange={this.changeHandler} defaultValue={this.props.selectedCard.note} ></textarea>
+          <textarea name="note" defaultValue={this.props.selectedCard.note} ></textarea>
           <button className="form-submit"> Submit</button>
         </form>
       </div>
